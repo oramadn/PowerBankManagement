eef44d7c58b6bc31de71febd6554bdb3099b0b87
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-18 14:38:33 +0300

    Added name and mobile nubmer for user registeration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :mobile_number, presence: true, length: { minimum: 14, maximum: 14 }
 end
