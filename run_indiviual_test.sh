0dd1521c9d975d8b986ae3b04aaac9f00aae5178
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-20 17:25:35 +0300

    Corrected None behaviour for station views

--- run_indiviual_test.sh
+++ run_indiviual_test.sh
@@ -4,4 +4,4 @@ rails db:schema:load RAILS_ENV=test
 rails db:fixtures:load RAILS_ENV=test
 
 # Run the specific test in power_bank_test.rb
-rails test test/models/station_test.rb
+rails test test/controllers/users/power_banks_controller_test.rb
