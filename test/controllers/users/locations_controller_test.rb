5b45c572c9594e1db238174ff4b851891d6e757f
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-20 18:23:18 +0300

    Added User Locations controller test unit

--- test/controllers/users/locations_controller_test.rb
+++ test/controllers/users/locations_controller_test.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class Users::LocationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
-    @users_location = users_locations(:user1)
+    @user = users(:user1)
+    sign_in @user
+    @location = locations(:location1)
   end
 
   test "should get index" do
@@ -10,39 +13,23 @@ class Users::LocationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_users_location_url
+  test "should show location" do
+    get users_locations_path(@location)
     assert_response :success
   end
 
-  test "should create users_location" do
-    assert_difference("Users::Location.count") do
-      post users_locations_url, params: { users_location: { lat: @users_location.lat, lng: @users_location.lng, name: @users_location.name } }
-    end
-
-    assert_redirected_to users_location_url(Users::Location.last)
+  test "should not create location" do
+    post users_locations_url, params: { location: { lat: 1.23, lng: 4.56, name: "Test Location" } }
+    assert_response :not_found
   end
 
-  test "should show users_location" do
-    get users_location_url(@users_location)
-    assert_response :success
+  test "should not update location" do
+    patch users_location_url(@location), params: { location: { lat: 1.23, lng: 4.56, name: "Updated Name" } }
+    assert_response :not_found
   end
 
-  test "should get edit" do
-    get edit_users_location_url(@users_location)
-    assert_response :success
-  end
-
-  test "should update users_location" do
-    patch users_location_url(@users_location), params: { users_location: { lat: @users_location.lat, lng: @users_location.lng, name: @users_location.name } }
-    assert_redirected_to users_location_url(@users_location)
-  end
-
-  test "should destroy users_location" do
-    assert_difference("Users::Location.count", -1) do
-      delete users_location_url(@users_location)
-    end
-
-    assert_redirected_to users_locations_url
+  test "should not destroy location" do
+    delete users_location_url(@location)
+    assert_response :not_found
   end
 end
