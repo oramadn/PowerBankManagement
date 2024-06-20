d4da8ce59ed6e9a02211741284d07bfed566b1c9
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-20 14:48:55 +0300

    user,station and warehouse id are nullable in powerbanks

--- db/migrate/20240618192512_create_power_banks.rb
+++ db/migrate/20240618192512_create_power_banks.rb
@@ -3,9 +3,9 @@ class CreatePowerBanks < ActiveRecord::Migration[7.1]
     create_table :power_banks do |t|
       t.decimal :charge
       t.string :status
-      t.references :user, null: false, foreign_key: true
-      t.references :station, null: false, foreign_key: true
-      t.references :warehouse, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
+      t.references :station, null: true, foreign_key: true
+      t.references :warehouse, null: true, foreign_key: true
 
       t.timestamps
     end
