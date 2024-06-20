8544f21a437e65f32e16b1da7ebe52230b45e2e0
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-20 19:44:58 +0300

    Added Pages test unit

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,18 +1,24 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get home" do
-    get pages_home_url
+    get root_path
     assert_response :success
   end
 
-  test "should get users" do
-    get pages_users_url
+  test "should get users page for user" do
+    user = users(:user1)  # Assuming you have a fixture or factory set up for users
+    sign_in user
+    get users_page_url
     assert_response :success
   end
 
-  test "should get admins" do
-    get pages_admins_url
+  test "should get admins page for admin" do
+    admin = admins(:admin1)  # Assuming you have a fixture or factory set up for admins
+    sign_in admin
+    get admins_page_url
     assert_response :success
   end
 end
