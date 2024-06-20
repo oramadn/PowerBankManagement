f59f080c9243a3d23c7c55bf620217728b17cc8d
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-20 19:06:12 +0300

    Added Admin Power_bank controller test unit

--- test/controllers/admins/power_banks_controller_test.rb
+++ test/controllers/admins/power_banks_controller_test.rb
@@ -1,48 +1,58 @@
 require "test_helper"
 
 class Admins::PowerBanksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @admins_power_bank = admins_power_banks(:one)
+    @admin = admins(:admin1)
+    @user = users(:user1)
+    sign_in @admin
+    @power_bank = power_banks(:power_bank1)
   end
 
   test "should get index" do
-    get admins_power_banks_url
+    get admins_power_banks_path
+    assert_response :success
+  end
+
+  test "should show power_bank" do
+    get admins_power_bank_path(@power_bank)
     assert_response :success
   end
 
   test "should get new" do
-    get new_admins_power_bank_url
+    get new_admins_power_bank_path
     assert_response :success
   end
 
-  test "should create admins_power_bank" do
-    assert_difference("Admins::PowerBank.count") do
-      post admins_power_banks_url, params: { admins_power_bank: { charge: @admins_power_bank.charge, station_id: @admins_power_bank.station_id, status: @admins_power_bank.status, user_id: @admins_power_bank.user_id, warehouse_id: @admins_power_bank.warehouse_id } }
+  test "should create power_bank" do
+    assert_difference("PowerBank.count") do
+      post admins_power_banks_path, params: { power_bank: { charge: 60.1, status: "available"} }
     end
+  
+    assert_redirected_to admins_power_banks_path
+  end  
 
-    assert_redirected_to admins_power_bank_url(Admins::PowerBank.last)
-  end
-
-  test "should show admins_power_bank" do
-    get admins_power_bank_url(@admins_power_bank)
-    assert_response :success
-  end
 
   test "should get edit" do
-    get edit_admins_power_bank_url(@admins_power_bank)
+    get edit_admins_power_bank_path(@power_bank)
     assert_response :success
   end
 
-  test "should update admins_power_bank" do
-    patch admins_power_bank_url(@admins_power_bank), params: { admins_power_bank: { charge: @admins_power_bank.charge, station_id: @admins_power_bank.station_id, status: @admins_power_bank.status, user_id: @admins_power_bank.user_id, warehouse_id: @admins_power_bank.warehouse_id } }
-    assert_redirected_to admins_power_bank_url(@admins_power_bank)
+  test "should update power_bank" do
+    patch admins_power_bank_path(@power_bank), params: { power_bank: { charge: "65", user_id: @user.id ,status: "reserved" } }
+    assert_redirected_to admins_power_banks_path(@power_bank)
+    @power_bank.reload
+    assert_equal 65.0, @power_bank.charge.to_f
+    assert_equal 1, @power_bank.user_id
+    assert_equal "reserved", @power_bank.status # Assinging a user reserves the power bank
   end
 
-  test "should destroy admins_power_bank" do
-    assert_difference("Admins::PowerBank.count", -1) do
-      delete admins_power_bank_url(@admins_power_bank)
+  test "should destroy power_bank" do
+    assert_difference("PowerBank.count", -1) do
+      delete admins_power_bank_path(@power_bank)
     end
 
-    assert_redirected_to admins_power_banks_url
+    assert_redirected_to admins_power_banks_path
   end
 end
