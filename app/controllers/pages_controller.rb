9c8930aa9688480222bdfc7376024c584a439341
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-21 17:09:36 +0300

    Configured Admins page frontend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  layout 'admin', only: [:admins]
   before_action :authenticate_user!, only: [:users], unless: :admin_signed_in?
   before_action :authenticate_admin!, only: [:admins]
 
