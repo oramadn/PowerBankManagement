f4d8bb5fd603f88590bb68bec1a93939a1519934
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-21 23:06:35 +0300

    Fixed Pages layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,30 @@
 class PagesController < ApplicationController
-  layout 'admin', only: [:admins]
-  layout 'user', only: [:users]
-  before_action :authenticate_user!, only: [:users], unless: :admin_signed_in?
+  before_action :set_layout
+
+  before_action :authenticate_user!, only: [:users]
   before_action :authenticate_admin!, only: [:admins]
 
   def home
+    # Home action logic here
   end
 
   def users
+    # Users action logic here
   end
 
   def admins
+    # Admins action logic here
+  end
+
+  private
+
+  def set_layout
+    if admin_signed_in?
+      self.class.layout 'admin'
+    elsif user_signed_in?
+      self.class.layout 'user'
+    else
+      self.class.layout nil  # No layout for non-authenticated users
+    end
   end
 end
