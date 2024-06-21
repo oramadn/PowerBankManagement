208801045cccb46dd935a9811e487880aaa63178
oramadn <omar.ramadanpersonal@gmail.com>
2024-06-21 20:48:26 +0300

    Fixed after sign up route for admins and users

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -50,10 +50,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    admins_page_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
